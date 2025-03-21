e717d40c0fce2cb38c678713201601841929fcf1
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-03-21 22:21:02 +0530

    created binary serach program

---
+++ 04_binarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key){
+    int start=0;
+    int end=size-1;
+    int mid=(end-start)/2;  //initialize mid to the middle of the array
+    while(start<=end){      //loop should run until start is less than or equal to end
+        if(arr[mid]==key){
+            return mid;     //returning index if element found
+        }
+        else if(key<arr[mid]){
+            end=mid-1;      
+        }
+        else{
+            start=mid+1;
+        }
+        mid=(start+end)/2;
+    }
+    return -1;              //if element not found return false(-1)
+}
+    
+
+int main(){
+    int even[10]={2,4,6,8,10,12,14,16,18,20};
+    int odd[9]={1,3,5,7,9,11,13,15,17};
+
+    cout<<binarySearch(even,10,16)<<endl;
+    cout<<binarySearch(odd,9,1)<<endl;
+
+}
